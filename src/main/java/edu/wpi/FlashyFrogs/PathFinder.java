05fa116c9bcd2f4970d1e3765beb0f941df706cb
Owen Krause <28845074+Owen-K@users.noreply.github.com>
2023-01-30 14:01:56 -0500

    Finished A star

--- src/main/java/edu/wpi/FlashyFrogs/PathFinder.java
+++ src/main/java/edu/wpi/FlashyFrogs/PathFinder.java
@@ -8,10 +8,7 @@ import lombok.NonNull;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.PriorityQueue;
+import java.util.*;
 
 @AllArgsConstructor
 public class PathFinder {
@@ -21,8 +18,9 @@ public class PathFinder {
     /**
      * Converts a String location name to the LocationName object associated with it.
      * MAY return Null if no location has that name
+     *
      * @param locationName the string location name to find the LocationName object of
-     * @param session the session to execute the query on
+     * @param session      the session to execute the query on
      * @return the LocationName object associated with the queried name
      */
     private LocationName longNameToLocation(@NonNull String locationName, @NonNull Session session) {
@@ -32,7 +30,8 @@ public class PathFinder {
     /**
      * converts a location name to a Node by looking for the latest Node that has the given location name.
      * Returns null if no result could be found
-     * @param name the location name to query for
+     *
+     * @param name    the location name to query for
      * @param session the session to run the query on
      * @return the found node, or null
      */
@@ -43,18 +42,19 @@ public class PathFinder {
         // Get unique result will either return the result, or null if there was none
         return session.createQuery(
                 """
-SELECT node
-FROM Move
-where location = :location
-ORDER BY moveDate DESC
-LIMIT 1
-""",
+                        SELECT node
+                        FROM Move
+                        where location = :location
+                        ORDER BY moveDate DESC
+                        LIMIT 1
+                        """,
                 Node.class).setParameter("location", name).uniqueResult();
     }
 
     /**
      * Gets the neighbors of a Node, by means of their edges
-     * @param node the node to get the neighbors of
+     *
+     * @param node    the node to get the neighbors of
      * @param session the session to use to get the results
      * @return the Set of Nodes that represent the Edges of a given Node
      */
@@ -64,51 +64,87 @@ LIMIT 1
         // of Nodes that this Node starts or ends. Select the opposite end in both cases.
         // Sets the parameter, and gets the result list as well
         return new HashSet<>(session.createQuery("""
-SELECT node1
-FROM Edge
-WHERE node2 = :node
-UNION
-SELECT node2
-FROM Edge
-WHERE node1 = :node
-""", Node.class).setParameter("node", node).getResultList());
+                SELECT node1
+                FROM Edge
+                WHERE node2 = :node
+                UNION
+                SELECT node2
+                FROM Edge
+                WHERE node1 = :node
+                """, Node.class).setParameter("node", node).getResultList());
     }
 
     /**
      * Public method to find the path between two locations.
      *
      * @param start the start node to find
-     * @param end the end node to find
+     * @param end   the end node to find
      * @return the path (as a list) between the two locations, or null if it could not find a path
      * @throws NullPointerException if the path between the two locations couldn't be found
      */
-    public List<Edge> findPath(@NonNull LocationName start, @NonNull LocationName end) {
+    public List<Node> findPath(@NonNull LocationName start, @NonNull LocationName end) {
+        Session session = sessionFactory.openSession();
         // Query location names and return nodes to send to aStar function
-        return null;
+        Node startNode = locationToNode(start, session);
+        Node endNode = locationToNode(end, session);
+
+        List<Node> path = aStar(startNode, endNode, session);
+
+        session.close();
+
+        return path;
     }
 
-    private List<Edge> aStar(@NonNull Node start, @NonNull Node end) {
+    private List<Node> aStar(@NonNull Node start, @NonNull Node end, @NonNull Session session) {
         PriorityQueue<NodeWrapper> openList = new PriorityQueue<>();
         List<NodeWrapper> closedlist = new LinkedList<>();
 
-        openList.add(new NodeWrapper(start, 0));
+        openList.add(new NodeWrapper(start, null, 0));
 
         // while open list is not empty
         while (!openList.isEmpty()) {
             NodeWrapper q = openList.poll();
-            // generate children of q and add them to openList
-            // for each child
-            // if goal then stop
-            // else compute g and h
-            // set f to g + h
-            // if node is already on the open list with lower f then skip
-            // if node is on closed list with lower f then skip else add to open list
+
+            if (q.node.equals(end)) {
+                List<Node> path = new LinkedList<>();
+                path.add(q.node);
+                while (!q.node.equals(start)) {
+                    q = q.parent;
+                    path.add(q.node);
+                }
+                Collections.reverse(path);
+                return path;
+            }
+
+            NODE_LOOP: for (Node node : getNeighbors(q.node, session)) {
+                NodeWrapper child = new NodeWrapper(node, q);
+                child.g = q.g + euclideanDistance(child.node, q.node);
+                child.h = euclideanDistance(child.node, end);
+                child.f = child.g + child.h;
+
+                for (NodeWrapper open : openList) {
+                    if (open.node.equals(child.node) && open.f < child.f) {
+                        break NODE_LOOP;
+                    }
+                }
+
+                for (NodeWrapper closed : closedlist) {
+                    if (closed.node.equals(child.node) && closed.f < child.f) {
+                        break NODE_LOOP;
+                    }
+                }
+                openList.add(child);
+            }
             closedlist.add(q);
         }
         return null;
     }
 
-    private double euclideanDistance(@NonNull int x1, @NonNull int x2, @NonNull int y1, @NonNull int y2) {
+    private double euclideanDistance(@NonNull Node node1, @NonNull Node node2) {
+        int x1 = node1.getXCoord();
+        int x2 = node2.getXCoord();
+        int y1 = node1.getYCoord();
+        int y2 = node2.getYCoord();
         int dx = x1 - x2;
         int dy = y1 - y2;
         return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
@@ -116,27 +152,27 @@ WHERE node1 = :node
 
     private class NodeWrapper implements Comparable<NodeWrapper> {
         Node node;
-        Node parent;
+        NodeWrapper parent;
         double g;
         double h;
         double f;
 
-        NodeWrapper(@NonNull Node node, @NonNull double cost) {
+        NodeWrapper(@NonNull Node node, NodeWrapper parent) {
             this.node = node;
-            this.parent = null;
-            this.f = cost;
+            this.parent = parent;
         }
 
-        NodeWrapper(@NonNull Node startNode, @NonNull Node endNode) {
-            node = endNode;
-            cost = euclideanDistance(startNode.getXCoord(), endNode.getYCoord(), endNode.getYCoord(), endNode.getYCoord());
+        NodeWrapper(@NonNull Node node, NodeWrapper parent, @NonNull double cost) {
+            this.node = node;
+            this.parent = parent;
+            this.f = cost;
         }
 
         @Override
         public int compareTo(NodeWrapper nodeWrapper) {
-            if(f==nodeWrapper.f)
+            if (f == nodeWrapper.f)
                 return 0;
-            else if(f>nodeWrapper.f)
+            else if (f > nodeWrapper.f)
                 return 1;
             else
                 return -1;
