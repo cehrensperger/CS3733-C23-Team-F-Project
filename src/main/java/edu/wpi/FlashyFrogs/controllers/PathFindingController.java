8af1752938d43ceafced7440aac5150b41a591dc
audreymongillo <ajmongillo@wpi.edu>
2023-01-30 22:33:02 -0500

    path finding controller

--- src/main/java/edu/wpi/FlashyFrogs/controllers/PathFindingController.java
+++ src/main/java/edu/wpi/FlashyFrogs/controllers/PathFindingController.java
@@ -28,6 +28,8 @@ public class PathFindingController {
   }
 
   public void handleGetPath(ActionEvent actionEvent) throws IOException {
-    //
+    String startPath = start.getText();
+    String endPath = end.getText();
+
   }
 }
