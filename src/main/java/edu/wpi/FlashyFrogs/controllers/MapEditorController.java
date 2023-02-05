f099636df2511dca23aca29c36c075020d1baead
Emre <emresbz@gmail.com>
2023-02-05 14:58:18 -0500

    Map EditorView and Controller

--- src/main/java/edu/wpi/FlashyFrogs/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/FlashyFrogs/controllers/MapEditorController.java
@@ -1,9 +1,38 @@
 package edu.wpi.FlashyFrogs.controllers;
 
+import static edu.wpi.FlashyFrogs.DBConnection.CONNECTION;
+
+import edu.wpi.FlashyFrogs.ORM.LocationName;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.hibernate.Session;
 
 /** Controller for the map editor, enables the user to add/remove/change Nodes */
-public class MapEditorController {
+public class MapEditorController implements Initializable {
+  public TableView<LocationName> locationTable;
+  public TableColumn<LocationName, String> longName;
+
   @FXML
-  private void initialize() {}
+  public void initialize(URL location, ResourceBundle resources) {
+    longName.setCellValueFactory(new PropertyValueFactory<>("longName"));
+
+    createLocationNameTable();
+  }
+
+  private void createLocationNameTable() {
+    Session session = CONNECTION.getSessionFactory().openSession();
+    List<LocationName> longNames =
+        session.createQuery("SELECT s FROM LocationName s", LocationName.class).getResultList();
+    ObservableList<LocationName> longNamesObservableList = FXCollections.observableList(longNames);
+    session.close();
+    locationTable.getItems().addAll(longNamesObservableList);
+  }
 }
