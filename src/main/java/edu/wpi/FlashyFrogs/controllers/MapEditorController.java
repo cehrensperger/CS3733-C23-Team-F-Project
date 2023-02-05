1bd592e311d2b54ee86aed7e2357750769f637c3
Ian W <49083526+Landaman@users.noreply.github.com>
2023-02-05 15:00:21 -0500

    MapEditorView floor dropdown

--- src/main/java/edu/wpi/FlashyFrogs/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/FlashyFrogs/controllers/MapEditorController.java
@@ -1,9 +1,28 @@
 package edu.wpi.FlashyFrogs.controllers;
 
+import edu.wpi.FlashyFrogs.ORM.Node;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+import lombok.SneakyThrows;
+
+import java.util.Objects;
 
 /** Controller for the map editor, enables the user to add/remove/change Nodes */
 public class MapEditorController {
+  public MFXComboBox<Node.Floor> floorSelector;
   @FXML
-  private void initialize() {}
+  private HBox editorBox; // Editor box, map is appended to this on startup
+  private MapController mapController; // Controller for the map
+
+  /**
+   * Initializes the map editor, adds the map onto it
+   */
+  @SneakyThrows
+  @FXML
+  private void initialize() {
+    FXMLLoader mapLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("views/Map.fxml")));
+    editorBox.getChildren().add()
+  }
 }
