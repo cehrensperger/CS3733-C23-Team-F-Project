646532834304da827afd4ca02df3a56c3658d72d
Ian W <49083526+Landaman@users.noreply.github.com>
2023-02-05 21:16:09 -0500

    Initial implementation of tooltips for the table

--- src/main/java/edu/wpi/FlashyFrogs/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/FlashyFrogs/controllers/MapEditorController.java
@@ -14,11 +14,13 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
+import javafx.util.Callback;
 import lombok.SneakyThrows;
 import org.controlsfx.control.PopOver;
 import org.hibernate.Session;
@@ -39,6 +41,55 @@ public class MapEditorController {
   private void initialize() {
     longName.setCellValueFactory(new PropertyValueFactory<>("longName"));
 
+    AtomicReference<PopOver> tablePopOver =
+        new AtomicReference<>(); // The pop-over the map is using for node highlighting
+
+    locationTable.setRowFactory(
+        new Callback<>() {
+          @Override
+          public TableRow<LocationName> call(TableView<LocationName> param) {
+            TableRow<LocationName> row = new TableRow<>(); // Create a new table row to use
+
+            // Add a listener to show the pop-up
+            row.hoverProperty()
+                .addListener(
+                    (observable, oldValue, newValue) -> {
+                      // If the pop over exists and is either not focused or we are showing a new
+                      // row
+                      if (tablePopOver.get() != null
+                          && (!tablePopOver.get().isFocused() || newValue)) {
+                        tablePopOver.get().hide(); // Hide the pop-over
+                        tablePopOver.set(null); // Delete the pop-over
+                      }
+
+                      // If we have a new value to show
+                      if (newValue) {
+                        // Load the location name info view
+                        FXMLLoader locationNameLoader =
+                            new FXMLLoader(
+                                getClass().getResource("../views/LocationNameInfo.fxml"));
+
+                        // Load the resource
+                        try {
+                          tablePopOver.set(
+                              new PopOver(locationNameLoader.load())); // Create the pop-over
+                        } catch (IOException e) {
+                          throw new RuntimeException(e); // If anything goes wrong, just re-throw
+                        }
+
+                        LocationNameInfoController controller =
+                            locationNameLoader.getController(); // Get the controller
+                        controller.setLocationName(
+                            row.getItem()); // Set the location name to the value
+
+                        tablePopOver.get().show(row); // Show the pop-over on the row
+                      }
+                    });
+
+            return row; // Return the generated row
+          }
+        });
+
     createLocationNameTable();
 
     // Load the map loader
@@ -48,7 +99,7 @@ public class MapEditorController {
     Pane map = mapLoader.load(); // Load the map
     editorBox.getChildren().add(map); // Put the map loader into the editor box
     mapController = mapLoader.getController(); // Load the map controller
-    AtomicReference<PopOver> popOver =
+    AtomicReference<PopOver> mapPopOver =
         new AtomicReference<>(); // The pop-over the map is using for node highlighting
 
     // Set the node creation processor
@@ -62,9 +113,9 @@ public class MapEditorController {
                     // If we're no longer hovering and the pop over exists, delete it. We will
                     // either create a new one
                     // or, keep it deleted
-                    if (popOver.get() != null && (!popOver.get().isFocused() || newValue)) {
-                      popOver.get().hide(); // Hide it
-                      popOver.set(null); // And delete it (set it to null)
+                    if (mapPopOver.get() != null && (!mapPopOver.get().isFocused() || newValue)) {
+                      mapPopOver.get().hide(); // Hide it
+                      mapPopOver.set(null); // And delete it (set it to null)
                     }
 
                     // If we should draw a new pop-up
@@ -75,7 +126,7 @@ public class MapEditorController {
 
                       try {
                         // Try creating the pop-over
-                        popOver.set(new PopOver(nodeInfoLoader.load()));
+                        mapPopOver.set(new PopOver(nodeInfoLoader.load()));
                       } catch (IOException e) {
                         throw new RuntimeException(e); // If it fails, throw an exception
                       }
@@ -84,7 +135,7 @@ public class MapEditorController {
                           nodeInfoLoader.getController(); // Get the controller to use
                       controller.setNode(node); // Set the node
 
-                      popOver.get().show(circle); // Show the pop-over
+                      mapPopOver.get().show(circle); // Show the pop-over
                     }
                   });
         });
@@ -106,7 +157,10 @@ public class MapEditorController {
     Session session = CONNECTION.getSessionFactory().openSession();
     List<LocationName> longNames =
         session.createQuery("FROM LocationName", LocationName.class).getResultList();
+
+    // Remove all locations that are have a node associated with them
     ObservableList<LocationName> longNamesObservableList = FXCollections.observableList(longNames);
+
     session.close();
     locationTable.getItems().addAll(longNamesObservableList);
   }
