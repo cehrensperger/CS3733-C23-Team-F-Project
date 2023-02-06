5827aa3deb4b13102091cdf7b4244ac1757582ad
Ian W <49083526+Landaman@users.noreply.github.com>
2023-02-06 14:41:50 -0500

    Made app startup work as expected

--- src/main/java/edu/wpi/FlashyFrogs/Fapp.java
+++ src/main/java/edu/wpi/FlashyFrogs/Fapp.java
@@ -27,7 +27,7 @@ public class Fapp extends Application {
     /* primaryStage is generally only used if one of your components require the stage to display */
     Fapp.primaryStage = primaryStage;
 
-    final FXMLLoader loader = new FXMLLoader(Fapp.class.getResource("views/MapEditorView.fxml"));
+    final FXMLLoader loader = new FXMLLoader(Fapp.class.getResource("views/main.fxml"));
     final Pane root = loader.load();
 
     Fapp.rootPane = root;
