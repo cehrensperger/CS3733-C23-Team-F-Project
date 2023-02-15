39b884325b663bc8456bfa56c78516b44715164f
Emre <emresbz@gmail.com>
2023-02-14 19:12:08 -0500

    Node Info can display multiple locations supposedly

--- src/main/java/edu/wpi/FlashyFrogs/MapEditor/MapEditorController.java
+++ src/main/java/edu/wpi/FlashyFrogs/MapEditor/MapEditorController.java
@@ -119,7 +119,6 @@ public class MapEditorController implements IController {
     Pane map = mapLoader.load(); // Load the map
     mapPane.getChildren().add(0, map); // Put the map loader into the editor box
     mapController = mapLoader.getController();
-    mapController.setFloor(Node.Floor.L1);
 
     // make the anchor pane resizable
     AnchorPane.setTopAnchor(map, 0.0);
@@ -137,6 +136,7 @@ public class MapEditorController implements IController {
     mapController.setNodeCreation(
         (node, circle) -> {
           // Set the on-click processor
+
           circle.setOnMouseClicked(
               (event) -> {
                 // If we're no longer hovering and the pop over exists, delete it. We will
@@ -178,7 +178,7 @@ public class MapEditorController implements IController {
         });
 
     floorSelector.setText("Floor " + Node.Floor.L1.name());
-
+    mapController.setFloor(Node.Floor.L1);
     // Add a listener so that when the floor is changed, the map  controller sets the new floor
     floorProperty.addListener(
         (observable, oldValue, newValue) -> {
