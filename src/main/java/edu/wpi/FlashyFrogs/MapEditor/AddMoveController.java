801caca953f8a087490bb567f974537a7821a791
Emre <emresbz@gmail.com>
2023-02-14 19:12:08 -0500

    Node Info can display multiple locations supposedly

--- src/main/java/edu/wpi/FlashyFrogs/MapEditor/AddMoveController.java
+++ src/main/java/edu/wpi/FlashyFrogs/MapEditor/AddMoveController.java
@@ -3,7 +3,7 @@ package edu.wpi.FlashyFrogs.MapEditor;
 import edu.wpi.FlashyFrogs.ORM.LocationName;
 import edu.wpi.FlashyFrogs.ORM.Move;
 import edu.wpi.FlashyFrogs.ORM.Node;
-import java.text.DateFormat;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 import javafx.collections.FXCollections;
@@ -46,7 +46,7 @@ public class AddMoveController {
    * @param event the event triggering this (unused)
    */
   @FXML
-  private void saveMove(ActionEvent event) {
+  private void saveMove(ActionEvent event) throws Exception {
 
     // If there are two moves on the same move and same location,
     // show and error message and don't add.
@@ -61,7 +61,8 @@ public class AddMoveController {
       LocationName location =
           session.find(LocationName.class, locationNameField.valueProperty().getValue());
       Node node = session.find(Node.class, nodeIDField.valueProperty().getValue());
-      Date date = DateFormat.getDateInstance().parse(moveDatePicker.getValue().toString());
+      Date date =
+          Date.from(moveDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
 
       // create the new move
       Move move = new Move(node, location, date);
@@ -78,6 +79,7 @@ public class AddMoveController {
       errorMessage.setText("This move already exists.");
     } catch (Exception e) {
       errorMessage.setText("Please fill all fields.");
+      throw (e);
     }
   }
 
