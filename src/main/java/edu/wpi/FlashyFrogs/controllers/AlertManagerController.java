49b0896b6b178d59ff333bad6bf45dc9d0f4f76c
Owen Krause <28845074+Owen-K@users.noreply.github.com>
2023-02-22 01:03:46 -0500

    alert refactor and announcement orm update

--- src/main/java/edu/wpi/FlashyFrogs/controllers/AlertManagerController.java
+++ src/main/java/edu/wpi/FlashyFrogs/controllers/AlertManagerController.java
@@ -1,15 +1,17 @@
 package edu.wpi.FlashyFrogs.controllers;
 
+import edu.wpi.FlashyFrogs.ORM.Announcement;
+import edu.wpi.FlashyFrogs.ORM.Department;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import org.controlsfx.control.SearchableComboBox;
 
-public class AlertsController {
+public class AlertManagerController {
       @FXML private TextField summaryField;
       @FXML private TextArea descriptionField;
-      @FXML private SearchableComboBox<Object> deptBox;
-      @FXML private ComboBox<Object> severityBox;
+      @FXML private SearchableComboBox<Department> deptBox;
+      @FXML private ComboBox<Announcement.Severity> severityBox;
 
 }
