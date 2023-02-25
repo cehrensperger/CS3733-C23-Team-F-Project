7c83c1bd3950efe91bf600fdf49d7ab860881df6
Ian W <49083526+Landaman@users.noreply.github.com>
2023-02-25 00:53:01 -0500

    Added JFeonix to Gradle

--- src/main/java/edu/wpi/FlashyFrogs/MoveVisualizer/MoveVisualizerController.java
+++ src/main/java/edu/wpi/FlashyFrogs/MoveVisualizer/MoveVisualizerController.java
@@ -14,7 +14,6 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.BiFunction;
-
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -224,15 +223,17 @@ public class MoveVisualizerController extends AbstractPathVisualizerController
     double[] mousePosition = new double[2];
     boolean[] dragEnabled = new boolean[1]; // Whether the drag is enabled
 
-      // Function that validates new X layout and scale
-      BiFunction<Double, Double, Boolean> validateX = (layoutX, scale) -> {
+    // Function that validates new X layout and scale
+    BiFunction<Double, Double, Boolean> validateX =
+        (layoutX, scale) -> {
           return true;
-      };
+        };
 
-      // Function that validates new Y layout and scale
-      BiFunction<Double, Double, Boolean> validateY = (layoutY, scale) -> {
+    // Function that validates new Y layout and scale
+    BiFunction<Double, Double, Boolean> validateY =
+        (layoutY, scale) -> {
           return true;
-      };
+        };
 
     node.setOnDragDetected(
         (event) -> {
@@ -385,7 +386,6 @@ public class MoveVisualizerController extends AbstractPathVisualizerController
 
             root.setScaleX(newScaleX);
             root.setLayoutX(startPosition[0] + shiftX);
-
 
             root.setScaleY(newScaleY);
             root.setLayoutY(startPosition[1] + shiftY);
