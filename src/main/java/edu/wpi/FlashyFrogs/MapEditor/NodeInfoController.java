d1dc5ca13afbfd5404bb2563fc7ce3409b11e5b6
Lindsey Mraz <68626727+lindseymraz@users.noreply.github.com>
2023-02-25 18:55:41 -0500

    Fix bug

--- src/main/java/edu/wpi/FlashyFrogs/MapEditor/NodeInfoController.java
+++ src/main/java/edu/wpi/FlashyFrogs/MapEditor/NodeInfoController.java
@@ -69,14 +69,14 @@ public class NodeInfoController {
       yCoordInt = Integer.parseInt(yCoord); // Parse y
     } catch (NumberFormatException error) {
       // If something went wrong, throw an exception indicating as much
-        Sound.ERROR.play();
+      Sound.ERROR.play();
       throw new IllegalArgumentException("Coordinates must be numeric!");
     }
 
     // If the coordinates are out of bound
     if (xCoordInt < 0 || xCoordInt > 9999 || yCoordInt < 0 || yCoordInt > 9999) {
+      Sound.ERROR.play();
       throw new IllegalArgumentException(
-              Sound.ERROR.play();
           "Coordinates must be 0 -> 9999!"); // Throw an exception indicating
     }
 
@@ -300,7 +300,7 @@ public class NodeInfoController {
                       .uniqueResult()
                   != null) {
             errorText.setText("A node with that ID already exists! No changes saved.");
-              Sound.ERROR.play();
+            Sound.ERROR.play();
             return; // Short-circuit, prevent submit
           }
 
