69946d467f5f8b045caa1050a5e4304727103ace
Lindsey Mraz <68626727+lindseymraz@users.noreply.github.com>
2023-02-25 19:01:37 -0500

    Fix bug

--- src/main/java/edu/wpi/FlashyFrogs/controllers/TeamMemberPopupController.java
+++ src/main/java/edu/wpi/FlashyFrogs/controllers/TeamMemberPopupController.java
@@ -1,10 +1,11 @@
 package edu.wpi.FlashyFrogs.controllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class TeamMemberPopupController {
 
-  private Text text;
+  @FXML private Text text;
 
   void setContent(String text) {
     this.text.setText(text);
