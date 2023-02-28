a0fe50b2d192e17190393128459423a4970990a2
Ian W <49083526+Landaman@users.noreply.github.com>
2023-02-28 15:15:45 -0500

    Refactored Pathfinding animation to MapController so that all map entities can use it

--- src/main/java/edu/wpi/FlashyFrogs/Map/MapController.java
+++ src/main/java/edu/wpi/FlashyFrogs/Map/MapController.java
@@ -12,6 +12,10 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
 import java.util.function.BiConsumer;
+
+import javafx.animation.Interpolator;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.Property;
@@ -23,12 +27,14 @@ import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -45,6 +51,14 @@ import org.hibernate.Session;
  */
 @GeneratedExclusion
 public class MapController {
+  @FXML private HBox animationPane;
+  @FXML private Circle cir1;
+  @FXML private Circle cir2;
+  @FXML private Circle cir3;
+  @FXML private Circle cir4;
+  @FXML private Circle cir5;
+  @FXML private Circle cir6;
+  private ParallelTransition parallelTransition = new ParallelTransition();
   @FXML private MFXButton upFloorButton;
   @FXML private MFXButton downFloorButton;
   @FXML private Label floorSelector; // Floor selector label
@@ -824,5 +838,83 @@ public class MapController {
     floorSelector.setVisible(!floorSelector.isVisible());
     floorSelectorButton.setVisible(!floorSelectorButton.isVisible());
     filterBox.setVisible(!filterBox.isVisible());
+  }
+
+  /**
+   * Starts the map loading animation, handling showing and starting it. This MUST be run in the UI Thread
+   */
+  public void startAnimation() {
+    parallelTransition.stop();
+
+    cir1.setVisible(true);
+    cir2.setVisible(true);
+    cir3.setVisible(true);
+    cir4.setVisible(true);
+    cir5.setVisible(true);
+    cir6.setVisible(true);
+    animationPane.setVisible(true);
+
+    // Create a TranslateTransition for each circle and add a delay
+    TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.2), cir1);
+    tt1.setInterpolator(Interpolator.EASE_BOTH);
+    tt1.setByY(-50);
+    tt1.setAutoReverse(true);
+    tt1.setCycleCount(2);
+    tt1.setDelay(Duration.seconds(0.0));
+    TranslateTransition tt2 = new TranslateTransition(Duration.seconds(0.2), cir2);
+    tt2.setInterpolator(Interpolator.EASE_BOTH);
+    tt2.setByY(-50);
+    tt2.setAutoReverse(true);
+    tt2.setCycleCount(2);
+    tt2.setDelay(Duration.seconds(0.2));
+    TranslateTransition tt3 = new TranslateTransition(Duration.seconds(0.2), cir3);
+    tt3.setInterpolator(Interpolator.EASE_BOTH);
+    tt3.setByY(-50);
+    tt3.setAutoReverse(true);
+    tt3.setCycleCount(2);
+    tt3.setDelay(Duration.seconds(0.4));
+    TranslateTransition tt4 = new TranslateTransition(Duration.seconds(0.2), cir4);
+    tt4.setInterpolator(Interpolator.EASE_BOTH);
+    tt4.setByY(-50);
+    tt4.setAutoReverse(true);
+    tt4.setCycleCount(2);
+    tt4.setDelay(Duration.seconds(0.6));
+    TranslateTransition tt5 = new TranslateTransition(Duration.seconds(0.2), cir5);
+    tt5.setInterpolator(Interpolator.EASE_BOTH);
+    tt5.setByY(-50);
+    tt5.setAutoReverse(true);
+    tt5.setCycleCount(2);
+    tt5.setDelay(Duration.seconds(0.8));
+    TranslateTransition tt6 = new TranslateTransition(Duration.seconds(0.2), cir6);
+    tt6.setInterpolator(Interpolator.EASE_BOTH);
+    tt6.setByY(-50);
+    tt6.setAutoReverse(true);
+    tt6.setCycleCount(2);
+    tt6.setDelay(Duration.seconds(1.0));
+
+    // Create a ParallelTransition to play the animations in parallel
+    parallelTransition = new ParallelTransition(tt1, tt2, tt3, tt4, tt5, tt6);
+    parallelTransition.setAutoReverse(true);
+    parallelTransition.setCycleCount(ParallelTransition.INDEFINITE);
+    // Start the animation
+    parallelTransition.playFromStart();
+  }
+
+  /**
+   * Handles stopping the map animation. Handles hiding and stopping it
+   */
+  public void stopAnimation() {
+    // stop the animation
+    parallelTransition.jumpTo(Duration.ZERO);
+    parallelTransition.stop();
+
+    // hide the circles
+    cir1.setVisible(false);
+    cir2.setVisible(false);
+    cir3.setVisible(false);
+    cir4.setVisible(false);
+    cir5.setVisible(false);
+    cir6.setVisible(false);
+    animationPane.setVisible(false);
   }
 }
