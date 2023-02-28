406bf4223433f59a853c4aa4a98cf9f3d7eb1efc
Ian W <49083526+Landaman@users.noreply.github.com>
2023-02-27 20:12:17 -0500

    Path class

--- src/main/java/edu/wpi/FlashyFrogs/TrafficAnalyzer/Path.java
+++ src/main/java/edu/wpi/FlashyFrogs/TrafficAnalyzer/Path.java
@@ -1,3 +1,13 @@
 package edu.wpi.FlashyFrogs.TrafficAnalyzer;
 
-class Path {}
+import edu.wpi.FlashyFrogs.ORM.LocationName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+
+@AllArgsConstructor
+class Path {
+    @Getter
+    @NonNull private LocationName startLocation; // Start location
+    @Getter @NonNull private LocationName endLocation; // End location
+}
